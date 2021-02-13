5e472773de522e6e7e9a3aef49cebfb8638a3257
Doğaç Eldenk <dogacel@gmail.com>
2021-02-14 01:57:05 +0300

    Fix rotations and some moves

--- src/consts.ts
+++ src/consts.ts
@@ -2,10 +2,6 @@ export enum Color {
     Wc = 0, Gc, Rc, Yc, Bc, Oc
 }
 
-export enum Rotation {
-    X = 0, X2, Xp, Y, Y2, Yp, Z, Z2, Zp
-}
-
 export enum FaceIndices {
     Ui = 0, Fi, Ri, Di, Bi, Li
 }
@@ -19,7 +15,11 @@ export enum Move {
     L, L2, Lp,
     M, M2, Mp,
     S, S2, Sp,
-    E, E2, Ep
+    E, E2, Ep,
+    X, X2, Xp,
+    Y, Y2, Yp,
+    Z, Z2, Zp,
+
 }
 
 export const relations = [
