d0ee3783b2627ae3fe3936ad96f1d590dd942bda
Doğaç Eldenk <dogacel@gmail.com>
2021-02-17 14:36:32 +0300

    Update rotation test

--- src/tests/rotation_test.ts
+++ src/tests/rotation_test.ts
@@ -17,45 +17,81 @@ describe('Solved rotates', () => {
     describe('X axis', () => {
         it('X rotates correctly', createAndMove(Move.X, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Fi], newFaces[Ui])
-            assert.notDeepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ui], newFaces[Bi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Di])
+            assert.deepEqual(oldFaces[Di], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ri], newFaces[Ri])
+            assert.deepEqual(oldFaces[Li], newFaces[Li])
         }));
         it('X\' rotates correctly', createAndMove(Move.Xp, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Fi], newFaces[Di])
-            assert.notDeepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Di], newFaces[Bi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Ui])
+            assert.deepEqual(oldFaces[Ui], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ri], newFaces[Ri])
+            assert.deepEqual(oldFaces[Li], newFaces[Li])
         }));
         it('X2 rotates correctly', createAndMove(Move.X2, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Fi], newFaces[Bi])
-            assert.notDeepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ui], newFaces[Di])
+            assert.deepEqual(oldFaces[Di], newFaces[Ui])
+            assert.deepEqual(oldFaces[Ri], newFaces[Ri])
+            assert.deepEqual(oldFaces[Li], newFaces[Li])
         }));
     });
 
     describe('Y axis', () => {
         it('Y rotates correctly', createAndMove(Move.Y, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Fi], newFaces[Li])
-            assert.notDeepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Li], newFaces[Bi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Ri])
+            assert.deepEqual(oldFaces[Ri], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ui], newFaces[Ui])
+            assert.deepEqual(oldFaces[Di], newFaces[Di])
         }));
         it('Y\' rotates correctly', createAndMove(Move.Yp, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Fi], newFaces[Ri])
-            assert.notDeepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ri], newFaces[Bi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Li])
+            assert.deepEqual(oldFaces[Li], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ui], newFaces[Ui])
+            assert.deepEqual(oldFaces[Di], newFaces[Di])
         }));
         it('Y2 rotates correctly', createAndMove(Move.Y2, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Fi], newFaces[Bi])
-            assert.notDeepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Ri], newFaces[Li])
+            assert.deepEqual(oldFaces[Li], newFaces[Ri])
+            assert.deepEqual(oldFaces[Ui], newFaces[Ui])
+            assert.deepEqual(oldFaces[Di], newFaces[Di])
         }));
     })
 
     describe('Z axis', () => {
         it('Z rotates correctly', createAndMove(Move.Z, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Ui], newFaces[Ri])
-            assert.notDeepEqual(oldFaces[Ui], newFaces[Ui])
+            assert.deepEqual(oldFaces[Ri], newFaces[Di])
+            assert.deepEqual(oldFaces[Di], newFaces[Li])
+            assert.deepEqual(oldFaces[Li], newFaces[Ui])
+            assert.deepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Bi])
         }));
         it('Z\' rotates correctly', createAndMove(Move.Zp, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Ui], newFaces[Li])
-            assert.notDeepEqual(oldFaces[Ui], newFaces[Ui])
+            assert.deepEqual(oldFaces[Li], newFaces[Di])
+            assert.deepEqual(oldFaces[Di], newFaces[Ri])
+            assert.deepEqual(oldFaces[Ri], newFaces[Ui])
+            assert.deepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Bi])
         }));
         it('Z2 rotates correctly', createAndMove(Move.Z2, (oldFaces, newFaces) => {
             assert.deepEqual(oldFaces[Ui], newFaces[Di])
-            assert.notDeepEqual(oldFaces[Ui], newFaces[Ui])
+            assert.deepEqual(oldFaces[Di], newFaces[Ui])
+            assert.deepEqual(oldFaces[Ri], newFaces[Li])
+            assert.deepEqual(oldFaces[Li], newFaces[Ri])
+            assert.deepEqual(oldFaces[Fi], newFaces[Fi])
+            assert.deepEqual(oldFaces[Bi], newFaces[Bi])
         }));
     });
 });
